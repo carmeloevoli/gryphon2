6ea700471dab85ceab39f437314fab9171913727
Carmelo Evoli <carmelo.evoli@gssi.it>
2023-08-01 15:19:43 +0200

    minor changes

--- include/gryphon.h
+++ include/gryphon.h
@@ -1,6 +1,13 @@
 #ifndef INCLUDE_GRYPHON_H
 #define INCLUDE_GRYPHON_H
 
+#include "gryphon/core/cgs.h"
+#include "gryphon/core/diffusion.h"
+#include "gryphon/core/event.h"
+#include "gryphon/core/input.h"
+#include "gryphon/core/sourceprofile.h"
+#include "gryphon/galaxy/galaxy.h"
+#include "gryphon/galaxy/uniform.h"
 #include "gryphon/utils/io.h"
 #include "gryphon/utils/logging.h"
 #include "gryphon/utils/lookupContainers.h"
@@ -8,5 +15,6 @@
 #include "gryphon/utils/progressbar.h"
 #include "gryphon/utils/random.h"
 #include "gryphon/utils/timer.h"
+#include "gryphon/utils/vector3.h"
 
 #endif  // INCLUDE_GRYPHON_H
